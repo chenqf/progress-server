abbf14cecef740203933c418589c85ec152f41e7
chenqf <qifeng.chen1006@foxmail.com>
2018-10-10 14:46:34 +0800

    add remark

---
+++ controllers/remark.js
@@ -0,0 +1,79 @@
+
+const controller = require('../lib/controller').factory(__filename);
+const db = require('../lib/mysql');
+const Sql = require('../lib/sql');
+const tool = require('../lib/tool');
+
+controller.requestMapping('/demo');
+
+
+controller.all('/insert', async (ctx,params,next) => {
+    let content = params.content;
+    if(!content){
+        throw new Error('缺少内容！')
+    }
+    let fkUserId = ctx.userId;
+    if(!fkUserId){
+        throw new Error('权限异常！')
+    }
+    let sql = new Sql('remark');
+    sql.set({fkUserId,content,createTime:Date.now()});
+    let data = await db.insert(sql);
+    ctx.body = data;
+});
+
+controller.all('/delete', async (ctx,params,next) => {
+    let id = params.id;
+    if(!id){
+        throw new Error('缺少id！')
+    }
+    let fkUserId = ctx.userId;
+    if(!fkUserId){
+        throw new Error('权限异常！')
+    }
+    let sql = new Sql('remark');
+    sql.whereEqual({id,fkUserId});
+    let data = await db.delete(sql);
+    ctx.body = data;
+});
+
+controller.all('/update', async (ctx,params,next) => {
+    let content = params.content;
+    let id = params.id;
+    if(!content){
+        throw new Error('缺少内容！')
+    }
+    if(!id){
+        throw new Error('缺少id！')
+    }
+    let fkUserId = ctx.userId;
+    if(!fkUserId){
+        throw new Error('权限异常！')
+    }
+    let sql = new Sql('remark');
+    sql.set({content}).whereEqual({id,fkUserId});
+    let data = await db.update(sql);
+    ctx.body = data;
+});
+
+
+controller.all('/query', async (ctx,params,next) => {
+    let fkUserId = ctx.userId;
+    if(!fkUserId){
+        throw new Error('权限异常！')
+    }
+    let sql = new Sql('remark');
+    let pre = params.pre || 0;
+    let start = tool.getTodayStart() - pre * 24 *60 * 60* 1000;
+    let end = tool.getTodayEnd() - pre * 24 *60 * 60* 1000;
+    sql.whereEqual({fkUserId}).whereGtEqual({createTime:start}).whereLtEqual({createTime:end});
+    let data = await db.query(sql);
+    ctx.body = data;
+});
+
+
+
+
+
+
+
