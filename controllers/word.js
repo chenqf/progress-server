8a1fee7f58792d09e2a37f5fd9b62bfcaa334a2b
chenqf <qifeng.chen1006@foxmail.com>
2018-10-10 09:27:48 +0800

    add word delete

--- controllers/word.js
+++ controllers/word.js
@@ -30,6 +30,9 @@ controller.post('/queryByPreDate', async (ctx,params,next) => {
     ctx.body = { items,totalCount:items.length };
 });
 
+/**
+ * 查询用户下的所有单词
+ */
 controller.post('/queryAll', async (ctx,params,next) => {
     let items =  await wordService.queryAll(ctx);
     ctx.body = { items,totalCount:items.length };
@@ -63,6 +66,9 @@ controller.post('/queryReviewNum', async (ctx,params,next) => {
 });
 
 
+/**
+ * 更新 user_word 的创建时间
+ */
 controller.post('/updateCreateTime', async (ctx,{id,createTime},next) => {
     if(!id || !createTime){
         throw new Error('缺少参数！')
@@ -72,6 +78,20 @@ controller.post('/updateCreateTime', async (ctx,{id,createTime},next) => {
     let data = await db.update(sql);
     ctx.body = data;
 });
+
+
+
+controller.post('/delete', async (ctx,params,next) => {
+    let fkUserId = ctx.userId;
+    if(!params.wordId){
+        throw new Error('缺少wordId')
+    }
+    let sql = new Sql('user_word');
+    sql.whereEqual({fkUserId,fkWordId:params.wordId});
+    let data = await db.delete(sql);
+    ctx.body = data;
+});
+
 
 
 
