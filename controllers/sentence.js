242fd868f8f3bd7efefae513eb036ea3bac7b5ba
chenqf <qifeng.chen1006@foxmail.com>
2018-10-24 18:09:59 +0800

    增加接口 例句相关

--- controllers/sentence.js
+++ controllers/sentence.js
@@ -19,6 +19,32 @@ controller.post('/insert', async (ctx,params,next) => {
     ctx.body = data;
 });
 
+controller.all('/delete', async (ctx,params,next) => {
+    let id = params.id;
+    if(!id){
+        throw new Error('缺少内容！')
+    }
+    let sql = new Sql('sentence');
+    sql.whereEqual({id});
+    let data = await db.delete(sql);
+    ctx.body = data;
+});
+
+controller.all('/update', async (ctx,params,next) => {
+    let content = params.content;
+    let explain = params.explain;
+    let id = params.id;
+    if(!id || !content || !explain){
+        throw new Error('缺少内容！')
+    }
+    let sql = new Sql('sentence');
+    sql.set({content,explain}).whereEqual({id:Number(id)});
+    let data = await db.update(sql);
+    ctx.body = data;
+});
+
+
+
 controller.post('/queryByPreDate', async (ctx,params,next) => {
     let sql = new Sql('sentence');
     let pre = Number(params.pre) || 0;
@@ -30,5 +56,19 @@ controller.post('/queryByPreDate', async (ctx,params,next) => {
         items
     };
 });
+
+controller.post('/queryAll', async (ctx,params,next) => {
+    let sql1 = new Sql('sentence');
+    let sql2 = new Sql('sentence');
+    let {startNum = 0,pageCount = 10} = params;
+    sql1.limit(startNum,pageCount);
+    let items = await db.query(sql1);
+    let count = await db.count(sql2);
+    ctx.body = {
+        items,
+        count
+    };
+});
+
 
 
